f51b40a95332d32779a82b2013f6bd7197a87d8e
Keygenqt <keygenqt@gmail.com>
2022-11-08 10:48:36 +0300

    Update docs

--- apps/react/frontend/src/App.js
+++ apps/react/frontend/src/App.js
@@ -12,14 +12,6 @@ function App() {
         <p>
           Frontend - {greeting}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
